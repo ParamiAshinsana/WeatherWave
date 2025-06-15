400d87536c3ce11688997698a867518197e1d07f
Parami <paramiashinsana@gmail.com>
2025-06-15 12:11:12 +0530

    added TopBar

---
+++ src/components/TopBar.tsx
@@ -0,0 +1,10 @@
+function TopBar() {
+    return (
+        <div className="flex ">
+            <div className="border size-14 grow">02</div>
+            <div className="border size-14 flex-none">03</div>
+        </div>
+    );
+}
+
+export default TopBar;
