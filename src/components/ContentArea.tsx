377ae567c37ce1257b39340bd368a0aaefdd697a
Parami <paramiashinsana@gmail.com>
2025-06-23 23:03:36 +0530

    updated

--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -16,7 +16,6 @@ function ContentArea() {
                     <TopBar onSearch={setCity}/>
                 </div>
 
-
                 <main className="p-2 sm:p-4 lg:p-6 grid grid-cols-1 lg:grid-cols-3 gap-4">
                     <div className="lg:col-span-1 flex flex-col gap-4">
                         <div className="rounded-xl overflow-hidden">
