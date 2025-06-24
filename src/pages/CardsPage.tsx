aeb98e88c66f55412b597337f184ac2b0e280508
Parami <paramiashinsana@gmail.com>
2025-06-24 11:14:26 +0530

    updated

--- src/pages/CardsPage.tsx
+++ src/pages/CardsPage.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import axios from 'axios';
 import {ArrowPathIcon, SunIcon, MoonIcon} from '@heroicons/react/24/solid';
 import { Link } from 'react-router-dom';
